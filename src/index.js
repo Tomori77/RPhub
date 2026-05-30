a02c0ef2a2c2853b3edbe4f9e2ea97ee1cba3f09
Tomori77 <16136745+tomori77@user.noreply.gitee.com>
2026-05-30 13:17:37 +0800

    修2

--- src/index.js
+++ src/index.js
@@ -432,13 +432,11 @@ async function serveStatic(request, env) {
     const pathname = new URL(request.url).pathname;
     const contentType = response.headers.get('content-type') || '';
     if (!shouldInject(pathname) || !contentType.includes('text/html')) return response;
-
-    const html = await response.text();
-    const injected = html.replace('</head>', INJECTED_BOOTSTRAP + '</head>');
-    return new Response(injected, {
-        status: response.status,
-        headers: response.headers
-    });
+    return new HTMLRewriter().on('head', {
+        element(element) {
+            element.append(INJECTED_BOOTSTRAP, { html: true });
+        }
+    }).transform(response);
 }
 
 export default {
